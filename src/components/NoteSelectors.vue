1c73107ee87f9cfa5004137170b442ab19780a3c
dani <dani.zaghian@gmail.com>
2022-02-17 10:03:02 -0800

    adds components for note container and note selectors

---
+++ src/components/NoteSelectors.vue
@@ -0,0 +1,17 @@
+<script>
+import NoteSelector from "@/components/NoteSelector.vue";
+
+export default {
+  components: {
+    NoteSelector
+  }
+};
+</script>
+
+<template>
+  <div class="note-selectors">
+    <NoteSelector />
+    <NoteSelector />
+    <NoteSelector />
+  </div>
+</template>
